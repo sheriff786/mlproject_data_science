5a0caa802160174bc9384a9eb021c3ca78f73371
sheriff786 <mohdsheriff27021996@gmail.com>
2023-09-15 11:50:07 +0530

    data transformation is done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,13 +2,18 @@
 Data ingestion file use to get the data from the storages,data sources,storage platform,live stream data
 '''
 
-import os,sys
+import os
+import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass  #use to create class variable 
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:   #----> use to save input data
@@ -50,5 +55,8 @@ class DataIngestion:
         
 if __name__ == "__main__":
    obj=DataIngestion()
-   obj.initiate_data_ingestion()
+   train_data,test_data=obj.initiate_data_ingestion()
+
+   data_transformation = DataTransformation()
+   data_transformation.initiate_data_transformation(train_data,test_data)
 
